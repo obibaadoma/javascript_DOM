180596733958490501b0e9622b4a3e99c120e837
obibaadoma <akrasikonadu@yahoo.com>
2024-02-03 11:56:12 +0000

    addition of js files

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+// const search = document.getElementById('search-books');
+// const bookList = document.getElementById('book-list');
+
+// console.log(search, bookList);
+
+// var titles = document.getElementsByClassName('title');
+
+// titles.forEach(function(item){
+// console.log(item);
+// })
+
+import {apiKey} from './util.js';
+
+console.log(apiKey);
